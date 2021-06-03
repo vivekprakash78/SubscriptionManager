85cf286c3cb0f66c6a26669e618dacda2da47fca
Vivek Prakash <vivekprakash78@gmail.com>
2021-06-03 23:58:55 +0530

    Added methods for CRUD operation from In-Memory DB

--- src/SubscriptionManager.Data.Service/ISubscriptionDbRepository.cs
+++ src/SubscriptionManager.Data.Service/ISubscriptionDbRepository.cs
@@ -1,7 +1,15 @@
-﻿namespace SubscriptionManager.Data
+﻿using SubscriptionManager.Data.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SubscriptionManager.Data
 {
     public interface ISubscriptionDbRepository
     {
-
+        Task<List<User>> GetAllUsers();
+        Task<User> GetUser(int id);
+        Task<User> AddUser(User entity);
+        Task<User> UpdateUser(User entity);
+        Task<User> DeleteUser(int id);
     }
 }
--- src/SubscriptionManager.Data.Service/SubscriptionDbRepository.cs
+++ src/SubscriptionManager.Data.Service/SubscriptionDbRepository.cs
@@ -1,4 +1,8 @@
-﻿namespace SubscriptionManager.Data
+﻿using SubscriptionManager.Data.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SubscriptionManager.Data
 {
     public class SubscriptionDbRepository : ISubscriptionDbRepository
     {
@@ -6,6 +10,31 @@
         public SubscriptionDbRepository(SubscriptionDbContext context)
         {
             _context = context;
+        }
+
+        public Task<User> AddUser(User entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> DeleteUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> GetUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<User> UpdateUser(User entity)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
